96e2396f3ed34e43cbcb5c941401c6946333e9d1
Yakeracles <271352565@qq.com>
2016-06-01 11:45:23 +0800

    yake
    第二个

---
+++ 第二个/第二个/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  第二个
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 Yake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
